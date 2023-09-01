42fcceae13332a77dfa0e66ec398ae755872814d
Brian-E <brian@intra.fo>
2023-09-01 08:16:26 +0100

    went back to original implementation. New implementation would have supported a bqn callback function, instead of a C one, but because of them being in a different thread than the main thread. I wanna come back to this though

--- examples/12_sound/callback.c
+++ examples/12_sound/callback.c
@@ -3,23 +3,38 @@
 // gcc callback.c -c -I/home/brian/CBQNStuffs/CBQN/include/ -fPIC
 // gcc -shared -o libcallback.so callback.o
 
-
 #include <raylib.h>
-#include <bqnffi.h>
+#include <math.h>           // Required for: sinf()
+// #include <stdio.h>         // Required for: printf()
 
-BQNV bqnFn;
+// Cycles per second (hz)
+float frequency = 440.0f;
+
+// Audio frequency, for smoothing
+float audioFrequency = 440.0f;
+
+// Index for audio rendering
+float sineIdx = 0.0f;
+
+typedef unsigned int u32;
+typedef short i16;
 
 // Audio input processing callback
-void AudioInputCallback(void *buffer, unsigned int frames) {
-    size_t len = frames;
-    BQNV arr = bqn_makeI16Vec(len, buffer);
-    BQNV bufferList = bqn_call1(bqnFn, arr);
-    bqn_free(arr);
-    bqn_readI16Arr(bufferList, buffer);
-    bqn_free(bufferList);
+void AudioInputCallback(i16 *buffer, u32 frames) {
+    audioFrequency = frequency+0.95f*(audioFrequency-frequency);
+    float incr = audioFrequency/44100.0f;
+
+    for (u32 i = 0; i < frames; i++) {
+        buffer[i] = (i16)(32000.0f*sinf(2*PI*sineIdx));
+        sineIdx += incr;
+        if (sineIdx > 1.0f) sineIdx -= 1.0f;
+    }
 }
 
-void *SetAudioInputCallback(BQNV func) {
-    bqnFn = func;
+void *GetCallbackPtr() {
     return AudioInputCallback;
+}
+
+void SetFreq(float x) {
+    frequency = x;
 }
