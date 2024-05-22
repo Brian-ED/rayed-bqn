c01c4601e9067db60db05281a249d6106ee2b4cc
Brian-ED <brianellingsgaard9@gmail.com>
2024-05-22 14:37:58 +0100

    added raylib installing methods makes it easier to install raylib

--- install-raylib/windows.ps1
+++ install-raylib/windows.ps1
@@ -1,5 +1,11 @@
 # Expects raylib folder to not exist
-curl -o temp.zip https://github.com/raysan5/raylib/releases/download/5.0/raylib-5.0_win64_msvc16.zip
-Expand-Archive -Force temp.zip ..\
-remove-Item temp.zip
-Rename-Item ..\raylib-5.0_win64_msvc16 -NewName raylib
+if (1 - (Test-Path -Path "$PSScriptRoot\..\raylib") ) {
+  $folder = "$PSScriptRoot\raylib-5.0_linux_amd64"
+  if (Test-Path -Path $folder ) {
+    Remove-Item -Path $folder -Recurse -Force
+  }
+  curl -o "$PSScriptRoot\temp.zip" https://github.com/raysan5/raylib/releases/download/5.0/raylib-5.0_win64_msvc16.zip
+  Expand-Archive -Force "$PSScriptRoot\temp.zip" "$PSScriptRoot\..\"
+  remove-Item "$PSScriptRoot\temp.zip"
+  Rename-Item "$PSScriptRoot\..\raylib-5.0_win64_msvc16" -NewName "raylib"
+}
