6350ac7bbd725e58d65305072c117d6b2578dcc7
Brian-ED <brianellingsgaard9@gmail.com>
2024-09-23 22:41:14 +0200

    Added VR visuals example

---
+++ examples/6_shaders/shaders/glsl330/default.fs
@@ -0,0 +1,11 @@
+#version 330
+in vec2 fragTexCoord;
+in vec4 fragColor;
+out vec4 finalColor;
+uniform sampler2D texture0;
+uniform vec4 colDiffuse;
+void main()
+{
+    vec4 texelColor = texture(texture0, fragTexCoord);
+    finalColor = texelColor*colDiffuse*fragColor;
+}
