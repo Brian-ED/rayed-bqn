8e45f5c2771a2d6b88a0bf4d0e5ccbb1bc5ca450
Brian-ED <brianellingsgaard9@gmail.com>
2024-10-24 20:36:13 +0200

    Big update, likely stuff broken, but have tested [gitignore] moved raylib install location
    [.gitmodules] updated bqn-libs, nothing changed except testing.
    
    [rayed.bqn]
    1. removed wrapping rayed.bqn in a namespace and giving 𝕊 to itself, instead I define stuff that needs rayed-bqn at the bottom.
    2. privated gamepadButton and gamepadAxisadded because soon there will be a controller namespace.
    3. Added git-submodules checking and error.
    4. removed autoparsing, moving it premenantly to the c-header-to-bqn-ffi repository. rayed-bqn will now only use the latest major raylib version (unless raylib.bqn is manually changed). Current raylib version in rayed-bqn is 5.0.
    5. removed the global namespace.
    6. added more error checks.
    7. removed the internal FileAt function.
    
    [examples]
    removed some •shows that were used for testing.
    removed some unused variables.
    
    [install.bqn]
    Improved massively, now sorting binary files in a lib folder and can install for windows, instead of using powershell (it does use a powershell script though).
    
    [image.bqn]
    removed leading spaces. removed global import, instead using •args.
    
    [rcore]
    uses •args insetead of global
    
    [loadconfig.bqn]
    removed config completely, don't need config anymore. Binary path can optionally be given as argument at importing rayed.bqn.
    
    [uncatagorized]
    removed c-header-to-bqn-ffi dependency and added raylib.bqn as a dependency.

--- install-raylib/windows.ps1
+++ install-raylib/windows.ps1
@@ -1,11 +1,2 @@
-# Expects raylib folder to not exist
-if (1 - (Test-Path -Path "$PSScriptRoot\..\raylib") ) {
-  $folder = "$PSScriptRoot\raylib-5.0_linux_amd64"
-  if (Test-Path -Path $folder ) {
-    Remove-Item -Path $folder -Recurse -Force
-  }
-  curl -o "$PSScriptRoot\temp.zip" https://github.com/raysan5/raylib/releases/download/5.0/raylib-5.0_win64_msvc16.zip
-  Expand-Archive -Force "$PSScriptRoot\temp.zip" "$PSScriptRoot\..\"
-  remove-Item "$PSScriptRoot\temp.zip"
-  Rename-Item "$PSScriptRoot\..\raylib-5.0_win64_msvc16" -NewName "raylib"
-}
+curl -o "$PSScriptRoot\temp.zip" https://github.com/raysan5/raylib/releases/download/5.0/raylib-5.0_win64_msvc16.zip
+Expand-Archive -Force "$PSScriptRoot\temp.zip" "$PSScriptRoot\..\"
