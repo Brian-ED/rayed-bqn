e8bd606eb925c37808cb38c48742eb6d24313dbc
Brian-ED <brianellingsgaard9@gmail.com>
2024-10-27 17:19:44 +0100

    install.bqn is now easier to use on windows one file for all platforms :D

--- install-raylib/windows.ps1
+++
@@ -1,2 +0,0 @@
-curl -o "$PSScriptRoot\temp.zip" https://github.com/raysan5/raylib/releases/download/5.0/raylib-5.0_win64_msvc16.zip
-Expand-Archive -Force "$PSScriptRoot\temp.zip" "$PSScriptRoot\..\"
