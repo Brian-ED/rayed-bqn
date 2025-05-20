0c49b9b7eec13567875567ceef5810ce2d3bfe77
Brian-ED <brianellingsgaard9@gmail.com>
2025-05-20 23:49:21 +0200

    Squashed commit of the following:
    commit fd80c359caa2f15f2715ebcdeac06cd4a98860a4
    Author: Brian-ED <brianellingsgaard9@gmail.com>
    Date:   Tue May 20 23:38:53 2025 +0200
    
        turns out i didn't need to build raylib
    
    commit 671c7ca2a85245c1dd53ddab97d9f3d1b69cef6f
    Author: Brian-ED <brianellingsgaard9@gmail.com>
    Date:   Tue May 20 23:35:15 2025 +0200
    
        Finally finished making a rayed-bqn flake
    
    commit 0ba89154daf9d0244cef2e8e1cb5c09dea835752
    Author: Brian-ED <brianellingsgaard9@gmail.com>
    Date:   Mon May 19 22:41:26 2025 +0200
    
        Reimplemented •file.Exists so it works
        CBQN's currently doesn't work for broken symlinks
    
    commit e36011ca835e5c60e6f145df1ab489318cfbb949
    Author: Brian-ED <brianellingsgaard9@gmail.com>
    Date:   Mon May 19 22:17:45 2025 +0200
    
        Flakes in progress!

---
+++ flake.nix
@@ -0,0 +1,57 @@
+# TODO try using raylib package from nixpkgs and modify hash and version 5.0→5.5
+
+{
+  description = "Rayed-bqn dev shell with BQN and raylib";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    BQN386     = { url = "github:dzaima/BQN386"      ; flake = false; };
+    bqn-libs   = { url = "github:mlochbaum/bqn-libs" ; flake = false; };
+    BQNoise    = { url = "github:mlochbaum/BQNoise"  ; flake = false; };
+    raylib-bqn = { url = "github:Brian-ED/raylib-bqn"; flake = false; };
+  };
+
+  outputs = {nixpkgs, BQN386, bqn-libs, BQNoise, raylib-bqn, ...}:
+  let
+    supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ];
+    forAllSystems = f: builtins.listToAttrs (map (system: {
+      name = system;
+      value = f system;
+    }) supportedSystems);
+  in {
+    packages = forAllSystems (system: let pkgs = nixpkgs.legacyPackages.${system}; in {
+      default = pkgs.stdenv.mkDerivation (finalAttrs: {
+        pname = "rayed-bqn";
+        version = "rolling";
+        meta = {
+          description = "Rayed BQN is a library made to write cross-platform applications using the BQN programming language. It inter-ops with raylib via FFI, but changes a lot of raylib's functions to be more in-lined with BQN's syntax.";
+          homepage = "https://brian-ed.github.io/rayed-bqn-docs/";
+          license = pkgs.lib.licenses.mit;
+          maintainers = [ ];
+          platforms = pkgs.lib.platforms.all;
+        };
+        src = ./.;
+        installPhase = ''
+          mkdir $out/
+          mkdir $out/imports
+          touch $out/install-raylib.bqn
+          cp rayed.bqn $out/
+          cp -r src $out/
+          mkdir $out/lib
+          ln -s ${BQN386    } $out/imports/BQN386
+          ln -s ${bqn-libs  } $out/imports/bqn-libs
+          ln -s ${BQNoise   } $out/imports/BQNoise
+          ln -s ${raylib-bqn} $out/imports/raylib-bqn
+          ln -s ${pkgs.raylib.outPath}/lib/libraylib.so.5.5.0 $out/lib/libraylib.so
+        '';
+      });
+      devShells.${system}.default = pkgs.mkShell {
+        name = "rayed-bqn-shell";
+        buildInputs = [
+          pkgs.cbqn
+        ];
+      };
+    });
+  };
+}
